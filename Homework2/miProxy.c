9574e9167fbfd74245fc5fe1ffb9baea3f8ca197
etsai7 <e.tsai70@gmail.com>
2018-02-20 18:30:00 -0500

    Command Line arguments for miProxy.c

--- Homework2/miProxy.c
+++ Homework2/miProxy.c
@@ -7,13 +7,52 @@
 #include <sys/select.h>
 #include <sys/types.h>
 
+#include <errno.h>
+
 static char  *Log;
 static float Alpha;
 static int   Listen_Port;
 static char  *DNS_IP;
-static int   DNS_Port
-static char  *www-ip;
+static int   DNS_Port;
+static char  *www_ip;
 
 /* Socket Setup */
 int sock;
 struct sockaddr_in sock_address;
+
+/* Methods */
+void Usage (int argc, char *argv[]);
+
+int main( int argc, char *argv[] ){
+	www_ip = "video.cse.umich.edu";
+
+	printf("\n");
+	Usage(argc, argv);
+	printf("\n");
+
+	return 0;
+}
+
+void Usage(int argc, char *argv[]){
+	if (argc < 6) {
+		perror("Not enough arguments\n");
+        exit(1);
+	}
+	Log = argv[1];
+	sscanf(argv[2], "%f", &Alpha);
+	sscanf(argv[3], "%d", &Listen_Port);
+	DNS_IP = argv[4];
+	sscanf(argv[5], "%d", &DNS_Port);
+	if(argc == 7){
+		www_ip = argv[6];
+	}
+
+	printf("--------------------MiProxy Information--------------------\n");
+	printf("Log Path:     %s\n", Log);
+	printf("Alpha:        %f\n", Alpha);
+	printf("Listen Port:  %d\n", Listen_Port);
+	printf("DNS IP:       %s\n", DNS_IP);
+	printf("DNS Port:     %d\n", DNS_Port);
+	printf("www-ip:       %s\n", www_ip);
+	printf("----------------------------------------------------------- ");
+}
